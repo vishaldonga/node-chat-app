a296080a51898c3a235109967f58563f11253071
Vishal Donga <vishaldonga811@gmail.com>
2021-06-24 22:14:44 +0530

    Add initial client and node-server code

---
+++ nodeServer/index.js
@@ -0,0 +1,28 @@
+//Node server for handling Socket IO connection
+
+const io = require("socket.io")(8001, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    console.log(name);
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("leave", users[socket.id]);
+    delete users[socket.id];
+  });
+});
